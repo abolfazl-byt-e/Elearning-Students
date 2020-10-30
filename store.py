39cb1a9bda1ffea66f8169e64d790fda9f8807ba
abolfazlbyte <abolfazl.byte@gmail.com>
2020-10-30 03:56:40 +0330

    create database and table in mysql

--- store.py
+++ store.py
@@ -1,9 +1,15 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password=""
-)
+    host="localhost",
+    user="root",
+    password="",
+    database='hakimqt',
+    charset='utf8',
+    use_unicode=True)
 
-print(mydb)
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE if not exists hakimqt")
+
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), code VARCHAR(255), role VARCHAR(255), lessons VARCHAR(255), img VARCHAR(255))")
